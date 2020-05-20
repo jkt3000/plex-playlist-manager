095f5f76aade1d83644b4d84f6d2e8cb451f6125
John Tajima <manjiro@gmail.com>
2020-05-19 23:56:49 -0400

    copy movie style over

--- src/Movie.svelte
+++ src/Movie.svelte
@@ -1,11 +1,20 @@
-<div class='bg-light border-1 mb-2 movie'>
-  <img src="{Plex.thumbUrl(movie.thumb, width, height)}" />
-  <p>{title} <small>({year})</small></p>
-  Rating: {movie.rating}
+
+
+<div class='cellItem draggable'>
+  <div class='poster-card'>
+    <img src="{Plex.thumbUrl(movie.thumb, width, height)}" class='poster'/>
+  </div>
+  <div class='poster-text'>
+    <h5>{title}</h5>
+    <p>{year}</p>
+<!--   Rating: {movie.rating}
   Duration: {parseInt(movie.duration/60/1000,10)}m
   Release: {moment(movie.originallyAvailableAt).format("MMM D, YYYY")}
-  
+ -->  
+
+  </div>
 </div>
+
 
 <script context='module'>
   import moment from 'moment';
@@ -14,8 +23,8 @@
 
 <script>
   export let movie;
-  let width=75;
-  let height=150;
+  let width=200;
+  let height=300;
   let key = movie.ratingKey;
   let title = movie.title;
   let year = movie.year;  
@@ -23,7 +32,7 @@
   let Plex = document.plex;
 </script>
 
-<style>
+<style lang='scss'>
 .movie {
   width:  150px;
   height:  300px;
@@ -35,4 +44,54 @@
   flex-shrink:  1;
 
 }
+
+
+.cellItem {
+  background: inherit;
+  box-sizing: border-box;
+  touch-action: none;
+  user-select: none;
+  margin:0.5em 0.5em 0 0;
+  display:inline-block;
+  width: 150px;
+  height: 270px;
+  overflow:hidden;
+  text-align:left; 
+  &.active {
+    border: 1px solid #aaa;
+    box-shadow: 5px 5px 10px 5px rgba(0,0,0,0.5);
+  }
+  &.tapped {
+    border: 1px solid red;
+  }
+  &.doubletapped {
+    box-shadow: 0px 0px 10px 5px rgba(255,255,0,0.75);
+  }
+  .poster-card {
+    width: 100%;
+  }
+  .poster-text {
+    height: 100px;
+    display:block;
+    overflow: hidden;
+    h5, p, span, a {
+      z-index: 1;
+      font-size: .9em;
+      line-height:1.5em;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      width: 100%;
+      margin:0; padding:0;
+      display:block;
+      overflow:hidden;
+    }
+    h5 { font-weight:bold; font-size: 1em; line-height: 1.5em;}
+  }
+
+  img.poster {
+    width: 150px; 
+  }  
+}
 </style>
+
+
